002a54bf9c2f1211ec32d9558e835bd507b3a12c
fahad-m-b <141445845+fahad-m-b@users.noreply.github.com>
2025-10-28 10:22:37 +0400

    rename pt2

--- Salalah Sports League/Program.cs
+++ Salalah Sports League/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace Salalah_Sports_League;
+﻿using _4_Projects.Salalah_Sports_League.Models;
 
-class Program
+namespace Salalah_Sports_League1
 {
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            League league = new League();
+
+            Team teamA = new Team("Salalah Goats", "Hussam");
+            teamA.AddPlayer(new Player("Ahmed", "Forward", 9));
+            teamA.AddPlayer(new Player("Salim", "Midfielder", 8));
+
+            Team teamB = new Team("Salalah Hawks", "Fahad");
+            teamB.AddPlayer(new Player("Saeed", "Forward", 10));
+            teamB.AddPlayer(new Player("Fayadh", "Goalkeeper", 1));
+
+            league.AddTeam(teamA);
+            league.AddTeam(teamB);
+
+            Match match1 = new Match(teamA, teamB);
+            match1.SetResult(2, 1);
+            league.RecordMatch(match1);
+
+            league.ShowAllMatches();
+            league.ShowStandings();
+        }
     }
 }
